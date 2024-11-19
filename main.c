a308c829149b48fffbd6b0d6737a2085e511f4cc
1acd810 <3503242715@qq.com>
2024-11-19 12:44:23 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,36 @@
 //请在下方输入你的代码：
+#include <stdio.h>
+
+
+
+int main(void)
+{
+	//正三角
+	for(int i=1;i<=9;i++)
+	{
+		for(int j=9;j>=i;j--)
+		{
+			printf("    ");//4位
+		}
+		for(int j=1;j<=i;j++)
+		{
+			printf("%dx%d=%2d  ",i,j,i*j);//8位
+		}
+		printf("\n");
+	}
+	printf("\n\n\n");
+	//倒三角
+	for(int i=9;i>=1;i--)
+		{
+			for(int j=9;j>=i;j--)
+			{
+				printf("    ");//4位
+			}
+			for(int j=1;j<=i;j++)
+			{
+				printf("%dx%d=%2d  ",i,j,i*j);//8位
+			}
+			printf("\n");
+		}
+	return 0;
+}
